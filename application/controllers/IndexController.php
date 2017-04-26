f6fe07b5c043927dcca9aff7c24aa99ca5870469
LeMayhem <lemay.michael@icloud.com>
2017-04-26 16:36:25 +0200

    [Offres]: requête de recherche de l’offre

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -13,4 +13,21 @@ class IndexController extends CI_Controller {
     $result = $this->rechercheCompetence->search($_POST['tag']);
     print_r(json_encode($result));
   }
+
+  public function findJobs(){
+    $this->load->model('rechercheCompetence');
+    $branche = $_POST['branche'];
+    $contrat = $_POST['contrat'];
+    $localisation = $_POST['localisation'];
+    $competences = explode("/", $_POST['competences']);
+
+
+    $offers = [];
+
+    foreach ($competences as $competence){
+      $result = $this->rechercheCompetence->getJobsFromForm($branche, $contrat, $localisation, $competence);
+      array_push($offers, $result);
+    }
+    echo $offers;
+  }
 }
--- application/models/rechercheCompetence.php
+++ application/models/rechercheCompetence.php
@@ -10,6 +10,21 @@ class rechercheCompetence extends CI_Model
 
   }
 
+  public function getJobsFromForm($branche, $contrat, $localisation, $competence){
+      $query = $this->db->query("SELECT offres.libelle, competences.label
+                                 FROM offres, competences, competences_offre
+                                 where competences_offre.id_competences = competences.id
+                                 AND competences_offre.id_offres = offres.id
+                                 and competences_offre.id_competences = 1
+                                 and offres.metier = '".$branche."'
+                                 AND offres.contrat = '".$contrat."'
+                                 AND offres.departement = '".$localisation."'
+
+                                 GROUP by offres.id ;");
+
+      return $query->result_array();
+  }
+
 
 }
 
