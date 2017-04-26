30620771317d1e4dd9a61e358008a7f1d4b10747
Victor Garcia <victorgarcia@Victor.local>
2017-04-26 10:53:58 +0200

    Champs de recherche de compétences

--- application/views/header.php
+++ application/views/header.php
@@ -10,11 +10,12 @@
 
 	<!-- Styles -->
 	<link href="<?php echo base_url(); ?>assets/css/style.css" rel="stylesheet" type="text/css">
+  <link href="<?php echo base_url(); ?>assets/css/tools.css" rel="stylesheet" type="text/css">
 
     <!-- Script JS -->
+    <script type="text/javascript" src='https://code.jquery.com/jquery-latest.min.js'></script>
 	<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/script.js"></script>
 
     <!-- Jquery Library -->
-    <script type="text/javascript" src='https://code.jquery.com/jquery-latest.min.js'></script>
-    
+
 </head>
