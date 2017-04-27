46791cf32cd085b959fc5e519ba96dea7ba53234
romainrichard08 <romain.richard.08@hotmail.fr>
2017-04-27 12:21:25 +0200

    dev test technique

---
+++ application/models/testTechnique.php
@@ -0,0 +1,44 @@
+<?php
+
+class testTechnique extends CI_Model
+{
+
+
+  public function getTestById($id_offre)
+  {
+		$query = $this->db->select('id_test')
+      					->from('offres')
+      					->where('id', $id_offre);
+
+		$query = $this->db->get();
+		return $query->row();
+
+  }
+
+  public function getQuestionsById($id_test)
+  {
+		$query = $this->db->select('*')
+      					->from('questions')
+      					->where('id_test', $id_test);
+
+		$query = $this->db->get();
+		return $query->result();
+
+  }
+
+
+   public function getReponsesById($id_question)
+  {
+		$query = $this->db->select('*')
+      					->from('reponses')
+      					->where('id_question', $id_question);
+
+		$query = $this->db->get();
+		return $query->result();
+
+  } 
+
+}
+
+
+
